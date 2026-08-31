0fe469b8613787f447a90f40d6cd24b9d68f3dad
Philip Mark Gutierrez <sensei_slayer@protonmail.com>
2026-08-31 15:08:17 +0800

    init

---
+++ src/test/java/net/groundgurus/blog_app_be/BlogAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.groundgurus.blog_app_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
